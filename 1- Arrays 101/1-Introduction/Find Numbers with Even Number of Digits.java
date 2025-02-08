464f581c6ff9a3d89e898910c4ada68d7b9cbd0a
Selin <63448943+selin-topcu@users.noreply.github.com>
2025-02-08 16:33:47 +0300

    Update Find Numbers with Even Number of Digits.java

--- 1- Arrays 101/1-Introduction/Find Numbers with Even Number of Digits.java
+++ 1- Arrays 101/1-Introduction/Find Numbers with Even Number of Digits.java
@@ -2,7 +2,7 @@ class Solution {
     public int findNumbers(int[] nums) {
         int count = 0;
         for (int i = 0; i < nums.length; i++) {
-            if ((int) (Math.log10(nums[i]) + 1) % 2 == 0) {
+            if (String.valueOf(nums[i]).length()) {
                 count++;
             }
         }
